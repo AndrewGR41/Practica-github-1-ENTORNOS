a704e4f6dd39e2b5da12e9701afc116966fc702a
admin <admin@Win7_1TB>
2019-12-17 11:09:50 +0300

    Primero

--- Ejemplo 7/src/ejemplo/pkg7/Ejemplo7.java
+++ Ejemplo 7/src/ejemplo/pkg7/Ejemplo7.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ejemplo.pkg7;
-
+import java.util.*;
 /**
  *
  * @author admin
@@ -16,6 +16,49 @@ public class Ejemplo7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double temperaturas[]=new double[7];
+        String diasSemana[]={"lunes","martes","miercoles","jueves","viernes","sabado","domingo"};
+        double suma=0,max=0,min=0;
+        int dia,posMax=0,posMin=0;
+        Scanner sc=new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        
+        System.out.println("Anota temperatura de la semana:");
+        
+        for(int i=0;i<temperaturas.length;i++){
+            System.out.println("Dia "+(i+1)+":");
+            System.out.println(diasSemana[i]+":");
+            temperaturas[i]=sc.nextDouble();
+            suma=suma+temperaturas[i];
+            if (i==0){
+                min=temperaturas[0];posMin=0;
+                max=temperaturas[0];posMin=0;
+            }
+            else{
+                if(temperaturas[i]>=max){
+                    max=temperaturas[i];
+                    posMax=i;
+                }
+                if(temperaturas[i]<=min){
+                    min=temperaturas[i];
+                    posMin=i;
+                }
+                
+            }
+        }
+        
+        System.out.println("La temperatura media ha sido: "+suma/7);
+        System.out.println("La maxima fue de "+max+" grados el "+diasSemana[posMax]);
+        System.out.println("El minimo fue de "+min+" grados el "+diasSemana[posMin]);
+        
+        do{
+            System.out.println("Anota dia del que quieras saber la temperatura:");
+            dia=sc.nextInt();
+        }while (dia<1 || dia>7);
+        
+        System.out.println("La temperatura fue "+temperaturas[dia-1]);
+        
+    
     }
     
 }
